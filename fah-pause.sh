24a5eb72c7c15f701b85197d4084c8b9a43c4bb1
Josh <josh@abellsmith.net>
2021-01-02 15:27:21 -0600

    Cleanup
    Did some minor cleanup and improvements

--- fah-pause.sh
+++ fah-pause.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 # To use this script, simply run it the same way you would run a sleep command, and it will pause Folding@home for the given amount of time.
+# The time defaults to one hour if none is given.
 # The process can be stopped at any time (using CTRL+C) to cancel the timer and resume Folding@home.
 
-if [ $# -gt 0 ]; then
-	for arg in "$@"; do
-		time=$(($time+$(sed 's/d/*24*3600 +/g; s/h/*3600 +/g; s/m/*60 +/g; s/s/\+/g; s/+[ ]*$//g' <<< $arg | bc)))
+time=3600
+
+if [ $# -gt 0 ]
+then
+	time=0
+	for arg in "$@"
+	do time=$(($time+$(sed 's/d/*24*3600 +/g; s/h/*3600 +/g; s/m/*60 +/g; s/s/\+/g; s/+[ ]*$//g' <<< $arg | bc)))
 	done
-else
-	time=3600
 fi
 
-echo -e "\e[31mPausing Folding@home for $(printf "$(($time/3600)):%02d:%02d" $((($time/60)%60)) $(($time%60))).\e[39m"
+printf "\e[31mPausing Folding@home for %d:%02d:%02d.\e[39m\\n" $(($time/3600)) $((($time/60)%60)) $(($time%60))
 trap 'echo -e "\nUnpausing Folding@home.";FAHClient -q --send-unpause &> /dev/null' EXIT
 
 FAHClient -q --send-pause &> /dev/null
-echo -n "Time remaining: $(printf "$(($time/3600)):%02d:%02d" $((($time/60)%60)) $(($time%60)))"
+printf "\\rTime remaining - %d:%02d:%02d" $(($time/3600)) $((($time/60)%60)) $(($time%60))
 
-while [ $time -gt 0 ]; do
+while [ $time -gt 0 ]
+do
 	time=$(($time-1))
-	echo -n -e "\\rTime remaining: $(printf "$(($time/3600)):%02d:%02d" $((($time/60)%60)) $(($time%60))) "
+	printf "\\rTime remaining - %d:%02d:%02d" $(($time/3600)) $((($time/60)%60)) $(($time%60))
 	sleep 1
 done
 
-echo -e "\\r\e[33mDelay has ended.\e[39m       "
+echo -e "\\r\e[33mDelay has ended.\e[39m        "
 echo "Unpausing Folding@home."
 FAHClient -q --send-unpause &> /dev/null
 
