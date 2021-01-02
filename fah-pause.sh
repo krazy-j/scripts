b2b59f453d6aea4d9ee520a4cc00908b599597ea
Josh <josh@abellsmith.net>
2021-01-02 03:13:43 -0600

    Big update
    Very significant improvements to the system and added a countdown timer.

--- fah-pause.sh
+++ fah-pause.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-delay=${1:-1h}
-echo -e "\e[31mStopping foldingathome for \e[4m$delay\e[24m.\e[39m"
-trap 'echo -e "\n\e[31mStop signal received.\e[39m\nResuming foldingathome.";systemctl start foldingathome' EXIT
-systemctl stop foldingathome
-sleep $delay
-echo -e "\e[33mDelay has ended.\e[39m"
-trap 'echo -e "Resuming foldingathome.";systemctl start foldingathome' EXIT
+if [ $# -gt 0 ]; then
+	for arg in "$@"; do
+		time=$(($time+$(sed 's/d/*24*3600 +/g; s/h/*3600 +/g; s/m/*60 +/g; s/s/\+/g; s/+[ ]*$//g' <<< $arg | bc)))
+	done
+else
+	time=3600
+fi
+
+echo -e "\e[31mPausing Folding@home for $(printf "$(($time/3600)):%02d:%02d" $((($time/60)%60)) $(($time%60))).\e[39m"
+trap 'echo -e "\nUnpausing Folding@home.";FAHClient -q --send-unpause &> /dev/null' EXIT
+
+FAHClient -q --send-pause &> /dev/null
+echo -n "Time remaining: $(printf "$(($time/3600)):%02d:%02d" $((($time/60)%60)) $(($time%60)))"
+
+while [ $time -gt 0 ]; do
+	time=$(($time-1))
+	echo -n -e "\\rTime remaining: $(printf "$(($time/3600)):%02d:%02d" $((($time/60)%60)) $(($time%60))) "
+	sleep 1
+done
+
+echo -e "\\r\e[33mDelay has ended.\e[39m       "
+echo "Unpausing Folding@home."
+FAHClient -q --send-unpause &> /dev/null
+
+trap EXIT
