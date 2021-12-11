c85b7518eb704b2dd2f7b76b064655799be424c9
Josh <josh@abellsmith.net>
2021-12-11 14:29:15 -0600

    Improvements and Optimization
    Improved the script and optimized certain aspects.

--- fah-pause.sh
+++ fah-pause.sh
@@ -1,60 +1,59 @@
 #!/bin/bash
 update=1
-quiet=false
 light=false
+exit_err() {
+	>&2 printf '%s: %s\nTry %s for more information.\n' "$0" "$1" "'$0 --help'"
+	exit 2
+}
 
-while (($#))
+while [[ $1 = -* ]]
 do
-	if [[ $1 = -* ]]
-	then
-		case $1 in
-			-h|--help)
-				echo "Usage: $0 [OPTION]... [NUMBER[SUFFIX]]..."
-				echo "Pause for NUMBER seconds. SUFFIX may be 's' for seconds (default), 'm' for minutes, 'h' for hours, or 'd' for days. NUMBER need not be an integer. Given two or more arguments, pause for the amount of time specified by the sum of their values. Given no arguments, pause for one hour."
-				echo "  -h, --help           display this help and exit"
-				echo "  -m, --minute-update  update the time left every minute instead of every second"
-				echo "  -q, --quiet          don't output any text"
-				echo "  -l, --light          don't output any text"
-				exit;;
-			-m|--minute-update) update=60;;
-			-q|--quiet) quiet=true;;
-			-q|--quiet) light=true;;
-			*)
-				echo "$0: unrecognized argument '$1'"
-				echo "Try '$0 --help' for more information."
-				exit 1
-		esac
-	elif [[ $1 =~ ^[0-9]+[smhd]?$ ]]
-	then ((delay+=$(sed "s/s//; s/m/*60/; s/h/*3600/; s/d/*86400/" <<< $1)))
-	else
-		echo "$0: invalid time interval '$1'"
-		echo "Try '$0 --help' for more information."
-		exit 1
-	fi
+	case $1 in
+		-h|--help)
+			echo "Usage: $0 [OPTION]... [NUMBER[SUFFIX]]..."
+			echo "Pause for NUMBER seconds. SUFFIX may be 's' for seconds (default), 'm' for minutes, 'h' for hours, or 'd' for days. NUMBER need not be an integer. Given two or more arguments, pause for the amount of time specified by the sum of their values. Given no arguments, pause for one hour."
+			echo "  -h, --help           display this help and exit"
+			echo "  -m, --minute-update  update the time every minute instead of every second"
+			echo "  -q, --quiet          don't output any text"
+			echo "  -l, --light          use just one sleep command for delay"
+			exit;;
+		-m|--minute-update) update=60;;
+		-q|--quiet) quiet=true;;
+		-l|--light) light=true;;
+		*) exit_err "invalid argument '$1'"
+	esac
 	shift
 done
+if (($#))
+then
+	for arg in $@
+	do [[ ! $arg =~ ^[0-9]+[smhd]?$ ]] && exit_err "invalid time interval '$arg'"
+	done
+	((delay=$(sed "s/ /+/g; s/s//g; s/m/*60/g; s/h/*3600/g; s/d/*86400/g" <<< $@)))
+else delay=3600
+fi
 
-((delay)) || delay=3600
-timeleft() { echo $(($1/3600))$(printf :%02d $((($1/60)%60)))$( ((update==60)) || printf :%02d $(($1%60))); }
+time_left() {
+	printf %d:%02d:%02d $(($1/3600)) $(($1%3600/60)) $((update<60?$1%60:0))
+}
 
-$quiet || printf "\e[31mPausing Folding@home for $(timeleft $delay)\e[39m\n"
-trap '$quiet && echo -e "\nUnpausing Folding@home"; FAHClient -q --send-unpause &> /dev/null' EXIT
+${quiet-printf '\e[31mPausing Folding@home for %s\e[39m\n' $(time_left $delay)}
+trap ${quiet-"printf '\nUnpausing Folding@home\n'; "}'FAHClient -q --send-unpause &> /dev/null' EXIT
 
 FAHClient -q --send-pause &> /dev/null
 
 if $light
 then sleep $delay
 else
-	$quiet || printf "Time remaining - $(timeleft $delay)"
 	((end=$(date +%s)+delay))
 	while (($(date +%s)<end))
 	do
+		${quiet-printf '\r\e[KTime remaining - %s' $(time_left $((end-$(date +%s))))}
 		sleep $update
-		$quiet || printf "\033[1K\rTime remaining - $(timeleft $((end-$(date +%s))))"
 	done
 fi
 
-$quiet || echo -e "\033[1K\r\e[33mDelay ended\e[39m\nUnpausing Folding@home"
-FAHClient -q --send-unpause &> /dev/null
+${quiet-printf '\r\e[K\e[33mDelay ended\e[39m\nUnpausing Folding@home\n'}
+FAHClient -q --send-unpause &>/dev/null
 
 trap EXIT
